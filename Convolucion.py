9b283d6deb511d940e458bcd1c1d2e4b782e9a12
Luis Paulo Flores <luisp.@MacBook-Air-de-Luis-2.local>
2021-03-19 13:38:17 -0600

    Segundo commit, archivo de convolucion agregado

---
+++ Convolucion.py
@@ -0,0 +1,51 @@
+"""
+Codigo tomado de https://github.com/adeveloperdiary/blog/tree/master/Computer_Vision/Sobel_Edge_Detection
+blog http://www.adeveloperdiary.com/data-science/computer-vision/how-to-implement-sobel-edge-detection-using-python-from-scratch/
+y modificado por Benjamin Valdes, después el codigo #main se modifico por Luis Paulo Flores para adaptarlo a la computadora usada.
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+ 
+def conv_helper(fragment, kernel):
+    """ multiplica 2 matices y devuelve su suma"""
+    
+    f_row, f_col = fragment.shape
+    k_row, k_col = kernel.shape 
+    result = 0.0
+    for row in range(f_row):
+        for col in range(f_col):
+            result += fragment[row,col] *  kernel[row,col]
+    return result
+
+def convolution(image, kernel):
+
+    image_row, image_col = image.shape #asigna alto y ancho de la imagen 
+    kernel_row, kernel_col = kernel.shape #asigna alto y ancho del filtro
+   
+    output = np.zeros(image.shape) #matriz donde guardo el resultado
+   
+    for row in range(image_row):
+        for col in range(image_col):
+                output[row, col] = conv_helper(
+                                    image[row:row + kernel_row, 
+                                    col:col + kernel_col],kernel)
+             
+    plt.imshow(output, cmap='gray')
+    plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
+    plt.show()
+
+    return output
+
+#MainCode
+ruta = ("Turquia.jpg")
+img = cv2.imread(ruta)
+print(img.shape)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(gray.shape)
+cv2.imshow('image', gray)
+kernel = np.ones((5,5))
+resultado = convolution(gray, kernel)
+
+"**codigo funcional, correr en terminal por problemas de librerias y cv2**"
